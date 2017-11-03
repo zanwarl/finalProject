425639a550d86f5629d60940e74e146e669b5b57
seotae <seotae@LAPTOP-RBVM1NK9>
2017-11-03 14:39:28 +0900

    seo

---
+++ finalProject/src/main/java/korea/member/model/MemberDAOImple.java
@@ -0,0 +1,32 @@
+package korea.member.model;
+
+import korea.member.model.MemberDTO;
+
+import org.mybatis.spring.SqlSessionTemplate;
+
+public class MemberDAOImple implements MemberDAO {
+	
+	private SqlSessionTemplate sqlMap;
+	
+	public MemberDAOImple(SqlSessionTemplate sqlMap) {
+		super();
+		this.sqlMap = sqlMap;
+	}
+
+	public int memberJoin(MemberDTO dto) {
+		int count=sqlMap.insert("memberInsert", dto);
+		return count;
+	}
+
+	public boolean memberSelect(String userid) {
+		String count=sqlMap.selectOne("memberSelect",userid);
+		if(count==null || count.equals("")) {
+			return false;
+		}else {
+			return true;
+		}
+	}
+
+	
+
+}
---
+++ finalProject/src/main/webapp/WEB-INF/views/member/memberMsg.jsp
@@ -0,0 +1,6 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<script>
+	window.alert('${memberDTO.member_id}ดิ ${msg}');
+	location.href='index.do';
+</script>
