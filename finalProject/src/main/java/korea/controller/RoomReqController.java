a14b30ecc33ce2eaab2006b1c8ae3b130f292597
user1 <user1@KH_PJ>
2017-11-09 17:24:56 +0900

    용국

--- finalProject/src/main/java/korea/controller/RoomReqController.java
+++ finalProject/src/main/java/korea/controller/RoomReqController.java
@@ -1,10 +1,14 @@
 package korea.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+import korea.roomreq.model.RoomreqDAO;
+import korea.roomreq.model.RoomreqDTO;
+
 import java.util.List;
 
 
